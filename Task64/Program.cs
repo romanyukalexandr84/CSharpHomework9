22acd132dad7b95cf934b9750d080be7ec635d3c
romanukalexandr <romanukalexandr@gmail.com>
2023-03-11 07:27:52 +0300

    add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿void Numbers (int count)
+{
+    if (count == 1) Console.Write (count);
+    else
+    {
+        Console.Write (count + ", ");
+        Numbers (count - 1);
+    }
+}
+
+int n;
+
+do
+{
+    Console.WriteLine ("Задайте натуральное число N");
+    n = Convert.ToInt32 (Console.ReadLine());
+} while (n <= 0);
+
+Console.WriteLine ("Натуральные числа в промежутке от N до 1:");
+Numbers(n);
+Console.WriteLine ();
